361e48bf11475d5c02b92de3ef3b685e36c01d12
ulfarj <ulfarj@gmail.com>
2018-04-01 15:28:46 +0000

    remove backup folder

--- src/_ backup/modules/index.js
+++
@@ -1,6 +0,0 @@
-import account from './account';
-import app from './app';
-import main from './main';
-import search from './search';
-
-export { account, app, main, search };
--- src/_ backup/styles/color.js
+++
@@ -1,7 +0,0 @@
-export const color = {
-  primary: '#03CEA4',
-  secondary: '#970747',
-  lightgrey: '#F2F2F2',
-  dark: '#000',
-  light: '#fff'
-};
