35f5167b33a66d117ea1014e35aa1e2da74c1880
ulfarj <ulfarj@gmail.com>
2018-04-02 15:53:09 +0000

    auth navigation

--- src/modules/app/Authenticated.js
+++ src/modules/app/Authenticated.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import AuthNavigation from '../navigation/AuthNavigation';
+import { color } from '../../styles/color';
 
-const Authenticated = ({ children }) => (<div>{ children }</div>);
+const NavWrapper = styled.div`
+  background: ${color.light};
+  position: sticky;
+  top: 0px;
+`;
+
+const Authenticated = ({ children }) => (
+  <div>
+    <NavWrapper>
+      <AuthNavigation />
+    </NavWrapper>
+    { children }
+  </div>
+);
 
 Authenticated.propTypes = {
   children: PropTypes.object.isRequired,
---
+++ src/modules/navigation/AuthNavigation.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled, { css, keyframes } from 'styled-components';
+import { push } from 'react-router-redux';
+import { connect } from 'react-redux';
+import { Flex, Box } from 'grid-styled';
+import Modal from 'react-modal';
+import Overlay from './component/Overlay';
+import { color } from '../../styles/color';
+import Logo from '../../assets/img/logo_big.svg';
+import * as actions from './actions';
+
+<Box
+  width={[
+    1/10,
+    2/10,
+    3/10,
+    4/10,
+    5/10,
+    6/10,
+    7/10,
+    8/10,
+    9/10,
+    10/10
+  ]}
+/>
+
+const Navgrid = styled(Flex)`
+  height: 100px;
+  border-bottom: 1px solid ${color.lightgrey} 
+  justify-content: space-between;
+  margin: 0 2.5vw;
+`;
+
+const Navitem = styled(Box)`
+  cursor: pointer;
+  align-self: center;
+  font-weight: 100;
+  font-size: 14px;
+`;
+
+const AuthNavigation = ({ dispatch }) => (
+  <Navgrid>
+    <Navitem width={[  1, 10/10, 3/10, 5/10, 6/10, 8/10 ]} onClick={() => dispatch(push('/'))}><img src={Logo} /></Navitem>
+    <Navitem onClick={() => dispatch(push('/search'))}>Find a Personal Shopper</Navitem>
+    <Navitem>Logout</Navitem>
+  </Navgrid>
+);
+
+AuthNavigation.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+
+export default connect()(AuthNavigation);
+
