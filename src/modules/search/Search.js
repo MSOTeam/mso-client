9cf442fbeb1dbfd8d183c04d465f3b0f6a89dd03
Björn Lárus <bjornlarus@gmail.com>
2018-04-13 10:25:40 +0200

    search

--- src/modules/search/Search.js
+++ src/modules/search/Search.js
@@ -15,13 +15,27 @@ const VideoOverlay = styled.div`
   overflow: hidden;
   height: 60vh;
   z-index:-1;
+  postition: relative;
 `;
+
+const SearchBox = styled.div`
+  position: absolute;
+  width: 500px;
+  height: 50px;
+  background: white;
+  z-index: 100;
+  transform: translate(-50%, -50%);
+  left: 50%;
+  top: 50%;
+`;
+
 
 
 class Search extends Component {
   render() {
     return (
     <VideoOverlay>
+      <SearchBox>Search</SearchBox>
       <video autoplay="true" loop="true" style={{minHeight:'100%', minWidth:'100%', position: 'relative', top: '50%', transform: 'translateY(-50%)'}}>
         <source src={ShopVideo} type="video/mp4"/>
         <SearchHeader />
