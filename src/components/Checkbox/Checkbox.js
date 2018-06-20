8a55a289d14d06d99abd453b86c869a5b85cc461
Björn Lárus Arnórsson <bjornlarus@gmail.com>
2018-06-20 00:18:17 +0000

    form

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -1,24 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Label = styled.label`
+  font-weight: 600;
+  font-size: 1.3em;
+  line-height: 35px;
+`;
+
+const CheckList = styled.div`
+  /* display: flex;
+  flex-wrap: wrap; */
+  margin-top: 10px;
+`;
+
+const Check = styled.div`
+  /* display: flex; */
+  /* width: calc(100%/4); */
+  margin-bottom: 15px;
+  /* &:last-child {
+    width: 100%;
+  } */
+`;
+
 
 const Checkbox = ({
   name, label, onChange, style, options,
 }) => (
-  <div style={{ paddingTop: 10 }}>
-    <label htmlFor={name} style={{ fontWeight: 600, fontSize: 18 }}>
+  <div>
+    <Label htmlFor={name}>
       {label}
-    </label>
-    {options.map(option => (
-      <div>
-        {option}
-        <input
-        type="checkbox"
-        name={option}
-        onChange={event => onChange(name, event)}
-        style={style}
-        />
-      </div>
-    ))}    
+    </Label>
+    <CheckList>
+      {options.map(option => (
+        <Check>
+          <input
+            type="checkbox"
+            name={option}
+            onChange={event => onChange(name, event)}
+            style={style}
+          />
+          {option}
+        </Check>
+      ))}
+    </CheckList>
   </div>
 );
 
--- src/modules/register/ShopperProfile.js
+++ src/modules/register/ShopperProfile.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import styled, { css, keyframes } from 'styled-components';
+
 import * as actions from './actions';
 import Input from '../../components/Input';
 import Checkbox from '../../components/Checkbox';
+
+const Wrapper = styled.div`
+  border-right: 1px #F7F7F7 solid;
+  border-left: 1px #F7F7F7 solid;
+  width: 50%;
+  margin: auto;
+
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column; 
+  margin: auto;
+  padding: 50px 50px 0 50px;
+`;
+
+const Section = styled.section`
+  display: flex;
+  margin-bottom: 30px;
+`;
 
 class ShopperProfile extends Component {
   constructor(props) {
@@ -43,103 +65,116 @@ class ShopperProfile extends Component {
 
   render() {
     return (
-      <form
-        onSubmit={this.onUpdate}
-        style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
-      >
-        <Input name="first_name" label="First name:" onChange={this.handleInputChange} />
-        <Input name="last_name" label="Last name:" onChange={this.handleInputChange} />
-        <Checkbox
-          name="languages"
-          label="The languages I speak are the following"
-          onChange={this.handleCheckboxChange}
-          options={
-            [
-              'English',
-              'Chinese',
-              'Spanish',
-              'Russian',
-              'French',
-              'Italian',
-              'German',
-              'Arabic',
-              'This information I do NOT consider necessary...',
-            ]
-          }
-        />
-        <Checkbox
-          name="locations"
-          label="Happy to meet with my clients at following location, such as:"
-          onChange={this.handleCheckboxChange}
-          options={
-            [
-              'Central London',
-              'South London',
-              'North London',
-              'West London',
-              'East London',
-              'This information I do NOT consider necessary...',
-              'Outside of London',
-            ]
-          }
-        />
-
-        <Input name="years_experience" label="Have been working as a Stylist or a Personal Shopper for ... years. (amount of years?):" onChange={this.handleInputChange} />
-        <Input name="about" label="Your One liner, What makes me unique:" onChange={this.handleInputChange} />
-        {/* hashtags */}
-        <Checkbox
-          name="styles"
-          label="The styles that I cover are the following"
-          onChange={this.handleCheckboxChange}
-          options={
-            [
-              'Vintage Fashion Style',
-              'Bohemian Fashion Style',
-            ]
-          }
-        />
-        <Checkbox
-          name="training"
-          label="I have completed the following training"
-          onChange={this.handleCheckboxChange}
-          options={
-            [
-              'Personal Shopping',
-              'Color Analysis',
-            ]
-          }
-        />
-        <Checkbox
-          name="assists_with"
-          label=" I will assist my clients with the following"
-          onChange={this.handleCheckboxChange}
-          options={
-            [
-              'Fashion Clothing',
-              'Outdoor Apparel'
-            ]
-          }
-        />
-        <Checkbox
-          name="store_types"
-          label="I will take my clients to"
-          onChange={this.handleCheckboxChange}
-          options={
-            [
-              'High street stores',
-              'High-End Stores / Boutiques',
-            ]
-          }
-        />
-        <Input name="hourly_price" label="Hourly price:" onChange={this.handleInputChange} />
-        {/* availability: {type: []},       */}
-        <Input name="hourly_price" label="Hourly price:" onChange={this.handleInputChange} />
-        <Input name="instagram" label="Instagram:" onChange={this.handleInputChange} />
-        <Input name="pinterest" label="Pinterest:" onChange={this.handleInputChange} />
-        <div>
-          <input type="submit" value="Submit" />
-        </div>
-      </form>
+      <Wrapper>
+        <Form onSubmit={this.onUpdate}>
+          <Section>
+            <Input name="first_name" label="First name:" onChange={this.handleInputChange} />
+            <Input name="last_name" label="Last name:" onChange={this.handleInputChange} />
+          </Section>
+          <Section>
+            <Checkbox
+              name="languages"
+              label="The languages I speak are the following"
+              onChange={this.handleCheckboxChange}
+              options={
+                [
+                  'English',
+                  'Chinese',
+                  'Spanish',
+                  'Russian',
+                  'French',
+                  'Italian',
+                  'German',
+                  'Arabic',
+                  'This information I do NOT consider necessary...',
+                ]
+              }
+            />
+          </Section>
+          <Section>
+            <Checkbox
+              name="locations"
+              label="Happy to meet with my clients at following location, such as:"
+              onChange={this.handleCheckboxChange}
+              options={
+                [
+                  'Central London',
+                  'South London',
+                  'North London',
+                  'West London',
+                  'East London',
+                  'Outside of London',
+                  'This information I do NOT consider necessary...',
+                ]
+              }
+            />
+          </Section>
+          <Section>
+            <Input name="years_experience" label="Have been working as a Stylist or a Personal Shopper for ... years. (amount of years?):" onChange={this.handleInputChange} />
+            {/* hashtags */}
+          </Section>
+          <Section>
+            <Input name="about" label="Your One liner, What makes me unique:" onChange={this.handleInputChange} />
+            {/* hashtags */}
+          </Section>
+          <Section>
+          <Checkbox
+            name="styles"
+            label="The styles that I cover are the following"
+            onChange={this.handleCheckboxChange}
+            options={
+              [
+                'Vintage Fashion Style',
+                'Bohemian Fashion Style',
+              ]
+            }
+          />
+          </Section>
+          <Section>
+          <Checkbox
+            name="training"
+            label="I have completed the following training"
+            onChange={this.handleCheckboxChange}
+            options={
+              [
+                'Personal Shopping',
+                'Color Analysis',
+              ]
+            }
+          />
+          </Section>
+          <Checkbox
+            name="assists_with"
+            label=" I will assist my clients with the following"
+            onChange={this.handleCheckboxChange}
+            options={
+              [
+                'Fashion Clothing',
+                'Outdoor Apparel'
+              ]
+            }
+          />
+          <Checkbox
+            name="store_types"
+            label="I will take my clients to"
+            onChange={this.handleCheckboxChange}
+            options={
+              [
+                'High street stores',
+                'High-End Stores / Boutiques',
+              ]
+            }
+          />
+          <Input name="hourly_price" label="Hourly price:" onChange={this.handleInputChange} />
+          {/* availability: {type: []},       */}
+          <Input name="hourly_price" label="Hourly price:" onChange={this.handleInputChange} />
+          <Input name="instagram" label="Instagram:" onChange={this.handleInputChange} />
+          <Input name="pinterest" label="Pinterest:" onChange={this.handleInputChange} />
+          <div>
+            <input type="submit" value="Submit" />
+          </div>
+        </Form>
+      </Wrapper>
     );
   }
 }
