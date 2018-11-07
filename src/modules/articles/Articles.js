b6b4ac13bc962fc087b5326c3e137f89b00a01b1
Björn Lárus Arnórsson <bjornlarus@gmail.com>
2018-11-07 10:30:12 +0000

    articles grid

--- src/modules/articles/Articles.js
+++ src/modules/articles/Articles.js
@@ -1,8 +1,62 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import styled, { css } from 'styled-components';
 import { PropTypes } from 'prop-types';
 import { push } from 'react-router-redux';
 import * as actions from './actions';
+import { color } from '../../styles/color';
+
+const ArticlesGrid = styled.div`
+  padding: 3% 70px 5% 140px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+  grid-gap: 1.5em;
+  
+  ${props => props.primary && css`
+    background: white;
+    color: palevioletred;
+  `}
+`;
+
+const ArticleBox = styled.div`
+  display: grid;
+`;
+
+const ArticleHeader = styled.div`
+  font-weight: 600;
+  font-size: 18px;
+  cursor: pointer;
+  line-height: 24px;
+  margin-bottom: 5px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 1;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+`;
+
+const ArticleExcerp = styled.div`
+  font-weight: 100;
+  font-size: 16px;
+  cursor: pointer;
+  line-height: 24px;
+  margin-bottom: 15px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 2;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+`;
+
+const ArticleTags = styled.span`
+  font-weight: 600;
+  font-size: 14px;
+  cursor: pointer;
+  line-height: 24px;
+  color: ${color.primary};
+  align-self: end;
+`;
 
 class Articles extends Component {
    componentDidMount = () => {
@@ -11,25 +65,20 @@ class Articles extends Component {
    }
 
    render() {
-     const { dispatch } = this.props;
+     const { dispatch } = this.props;     
      const articles = this.props.articles.map(article => (
-       <div
-         onClick={() => dispatch(push(`${'/article/'}${article._id}`))}
-         style={{ paddingBottom: 20 }}
-       >
-         <div style={{ fontWeight: 600, fontSize: 18, paddingBottom: 20 }}>
-           {article.tags.map(tag => (<span style={{ paddingRight: 8 }}>{tag}</span>))}
-         </div>
-         <div style={{ fontWeight: 600, fontSize: 20, cursor: 'pointer' }}>{article.title}</div>
-         <div dangerouslySetInnerHTML={{ __html: article.excerpt }} />
-         <div>Length: {article.length}</div>
-       </div>
+       <ArticleBox onClick={() => dispatch(push(`${'/article/'}${article._id}`))}>         
+         <ArticleHeader>{article.title}</ArticleHeader>
+         <ArticleExcerp dangerouslySetInnerHTML={{ __html: article.excerpt }} />
+         {/* <div>Length: {article.length}</div> */}
+         {article.tags.map(tag => (<ArticleTags>#{tag}</ArticleTags>))}
+       </ArticleBox>
      ));
 
      return (
-       <div style={{ padding: '3% 70px 5% 140px' }}>
+       <ArticlesGrid>
          {articles}
-       </div>
+       </ArticlesGrid>
      );
    }
 }
