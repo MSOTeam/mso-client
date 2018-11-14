f7c3684dadc0f3e04539b3c9f5df52627a25c0e8
Björn Lárus Arnórsson <bjornlarus@gmail.com>
2018-11-14 14:27:10 +0000

    edit article

--- src/modules/articles/Article.js
+++ src/modules/articles/Article.js
@@ -3,6 +3,10 @@ import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
 import styled, { css } from 'styled-components';
 import * as actions from './actions';
+import Edit from '../../assets/edit.svg';
+import Highlight from '../../assets/highlight.svg';
+import CommentMsg from '../../assets/comment.svg';
+import Members from '../../assets/members.svg';
 
 const StatBox = styled.div`
   display: flex;
@@ -48,6 +52,63 @@ const ArticleText = styled.p`
   }
 `;
 
+const EditBox = styled.div`
+  position: fixed;
+  width: calc(100vw - 80px);
+  background:#FFF6D6;
+  bottom: 0;
+  right: 0;
+  height: 70px;
+  display: flex;
+  justify-content: center;
+`;
+
+const EditWrapper = styled.div`
+  display: flex;
+  width: 40%;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const EditItem = styled.div`
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-size: 14px;
+  color: #5649CF;
+  ${props => props.edit && css`
+    &::before {
+      content: url(${Edit});
+      margin-right: 10px;
+      position: relative;
+      top: 2px;
+    }
+  `}
+  ${props => props.highlight && css`
+    &::before {
+      content: url(${Highlight});
+      margin-right: 10px;
+      position: relative;
+      top: 2px;
+    }
+  `}
+  ${props => props.comment && css`
+    &::before {
+      content: url(${CommentMsg});
+      margin-right: 10px;
+      position: relative;
+      top: 2px;
+    }
+  `}
+  ${props => props.members && css`
+    &::before {
+      content: url(${Members});
+      margin-right: 10px;
+      position: relative;
+      top: 2px;
+    }
+  `}
+`;
+
 class Article extends Component {
   componentDidMount = () => {
     const { dispatch, match } = this.props;
@@ -70,6 +131,14 @@ class Article extends Component {
           </StatBox>
           <ArticleText dangerouslySetInnerHTML={{ __html: article.content }} />
         </div>
+        <EditBox>
+          <EditWrapper>
+            <EditItem edit>Edit</EditItem>
+            <EditItem highlight>Highlight</EditItem>
+            <EditItem comment>Comment</EditItem>
+            <EditItem members>Members</EditItem>
+          </EditWrapper>
+        </EditBox>
       </div>
     );
   }
